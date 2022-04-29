faec46fb74408315f665a2bfa481364118688b70
rafhael <rafhael.martins@iesb.edu.br>
2022-04-29 00:47:11 +0000

    :sparkles: add: Script POC 01 IESB

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -7,16 +7,59 @@ echo
 
 #PASSO 01
 echo "----- [PASSO 01] Atualizando pacotes -----"
-sudo apt-get update
-sudo apt-get upgrade
+apt-get update
+apt-get upgrade
 echo
 
 #PASSO 02
 echo "----- [PASSO 02] Instalando pacote (apache2) -----"
-sudo apt-get install -y apache2
+apt-get install -y apache2
 echo
 
 #PASSO 03
 echo "----- [PASSO 03] Instalando pacote (wget) -----"
-sudo apt-get install -y wget
+apt-get install -y wget
 echo
+
+#PASSO 04
+echo "----- [PASSO 04] Download do arquivo .zip -----"
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+echo
+
+#PASSO 05
+echo "----- [PASSO 05] Instalando pacote (zip e unzip) -----"
+apt-get install -y zip unzip
+echo
+
+#PASSO 06
+echo "----- [PASSO 06] Descompactar arquivo .zip -----"
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+echo
+
+#PASSO 07
+echo "----- [PASSO 07] Copiando arquivo descompactado para o diretório (/var/www/html/) -----"
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035 
+cp * /var/www/html/
+echo
+
+#PASSO 08
+echo "----- [PASSO 08] Entrando na pasta (/var/www/html/) -----"
+cd /var/www/html/
+echo
+
+#PASSO 09
+echo "----- [PASSO 09] Removendo arquivo index existente na pasta -----"
+rm index.html
+echo
+
+#PASSO 10
+echo "----- [PASSO 10] Renomea o arquivo (cv-resumido.html) -----"
+mv cv-resumido.html index.html
+echo
+
+#PASSO 11
+echo "----- [PASSO 11] Iniciar servidor web Apache -----"
+/etc/init.d/apache2 start
+echo
+
+echo "----- FIM -----"
