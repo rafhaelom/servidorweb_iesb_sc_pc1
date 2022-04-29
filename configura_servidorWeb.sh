9222595b7b0cd44a7a8971c9045509912adaf5a6
rafhael <rafhael.martins@iesb.edu.br>
2022-04-29 00:07:42 +0000

    :sparkles: add: Comando de instalação do pacote wget

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 # Prova de Conceito 01 - Rafhael de Oliveira Martins
-
 echo "POC 01 IESB - Rafhael de Oliveira Martins"
+echo
+echo
 
-# ETAPA 01
-echo "[ETAPA 01]"
+#PASSO 01
+echo "----- [PASSO 01] Atualizando pacotes -----"
+sudo apt-get update
+sudo apt-get upgrade
+echo
+
+#PASSO 02
+echo "----- [PASSO 02] Instalando pacote (apache2) -----"
+sudo apt-get install -y apache2
+echo
+
+#PASSO 03
+echo "----- [PASSO 03] Instalando pacote (wget) -----"
+sudo apt-get install -y wget
+echo
